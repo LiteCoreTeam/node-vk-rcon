47cf38e87d3268cf587b2e96ea9b822d3b5705bd
Dmitry Lisov <zxcfox@proton.me>
2024-04-13 14:33:53 +0300

    1.0.0-null\

---
+++ bot.js
@@ -0,0 +1,12 @@
+/**
+ * @author LiteCoreTeam
+ * @version 1.0.0
+ * @github https://github.com/LiteCoreTeam/node-vk-rcon
+ * @vk https://vk.com/litecore_team
+ * @telegram https://t.me/litecoreteam
+ */
+
+const config = require("./config.json");
+const rcon = require("./src/MinecraftRCON");
+const VK = require("vk-io");
+const vk = new VK({});
