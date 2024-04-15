3545439e48d8c9b132c74cf9cc4b872f0a68ab83
Dmitry Lisov <zxcfox@proton.me>
2024-04-15 13:08:03 +0300

    ок.

--- bot.js
+++ bot.js
@@ -1,14 +1,10 @@
-/**
- * @author LiteCoreTeam
- * @version 1.0.0
- * @github https://github.com/LiteCoreTeam/node-vk-rcon
- * @vk https://vk.com/litecore_team
- * @telegram https://t.me/litecoreteam
- */
-
+const allowedUsers = [1,2,3]; //Айдишники вк тех, кому можно юзать бота
+const notAllowedCommands = ["op", "deop", "stop"]; //Команды которые нельзя использовать
 const config = require("./config.json");
+
 const RCON = require("./src/MinecraftRCON");
-let 
+const rcon = new RCON(config.rcon.address, config.rcon.port, config.rcon.password);
+
 const { VK } = require("vk-io");
 const vk = new VK({
     token: config.access_token,
@@ -16,16 +12,36 @@ const vk = new VK({
 });
 
 vk.updates.on(["message_new"], async (context)=> {
-    if(context.senderId < 0) {
-        return;
-    }
-
+    if(context.senderId < 0) return;
     if(context.text.indexOf("/")) {
-        let string = context.text.substring(1);
+        let args = (context.text.substring(1)).split(" ");
+        let commandName = args.shift();
+        if(args.length == 0) args = null;
+
+        if(allowedUsers.includes(context.senderId)) {
+            context.send("Вы не можете отправлять запросы к RCON сервера!");
+            return;
+        }
+    
+        if(notAllowedCommands.includes(commandName)) {
+            context.send("Использование данной команды запрещено!");
+            return;
+        }
+
         let connection = rcon.connect();
+        let command = (`${commandName} ${args.slice(" ")}`).trim();
+        rcon.sendCommand(connection, command);
+        rcon.disconnect(connection);
     }
 });
 
 vk.updates.start().then(()=> {
     console.log("RCON-Бот успешно запущен!");
+    console.log(`
+* @author LiteCoreTeam
+* @version 1.0.0
+* @github https://github.com/LiteCoreTeam/node-vk-rcon
+* @vk https://vk.com/litecore_team
+* @telegram https://t.me/litecoreteam
+`);
 });
